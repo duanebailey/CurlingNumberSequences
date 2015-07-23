3f56e165501536986e205c1907a2fb8eb70f890a
Adly <adlytempleton@gmail.com>
2015-07-23 09:39:16 -0400

    Added sl.py and updated expand.py and freq.py to use S-L notation

--- expand.py
+++ expand.py
@@ -1,9 +1,8 @@
-"""Translates 1s and 3s, where each digit represents the number of 2s in between each 3, and 1=1332, into normal strings"""
+"""Translates S and L, where each digit represents the number of 2s in between each 3, and S=3, L=1332, into normal strings"""
 while True:
     data = input()
-    data = data.replace("1332", "1")
     #Note the careful ordering of replacements
-    data = data.replace("3", "2223")
-    data = data.replace("1", "2322232223223")
+    data = data.replace("S", "2223")
+    data = data.replace("L", "2322232223223")
     data = "3" + data
     print(data)
